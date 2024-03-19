71f3c7c5a3f6298b068adafffb4cb37d60d03150
Shuja Ur Rehman <shuja0094@gmail.com>
2024-03-19 11:32:17 +0500

    chnages'

--- src/App.js
+++ src/App.js
@@ -1,69 +1,33 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { GeneralLayout } from "./themes/GenralLayout";
-import { BackgroundImage, CaseStudy, MobileNav } from "./components";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { BackgroundImage, MobileNav } from "./components";
+import { Routes, Route, useParams, useLocation } from "react-router-dom";
 import { CustomCursor } from "./themes/CustomCursor";
-import instance from "./api";
 function App() {
   const [data, setData] = useState(null);
+  const [id, setId] = useState(null);
+  const location = useLocation();
+  const BASE_URL =
+    process.env === "local" ? "localhost:5000/api/v1/developer" : "/";
   const getPortfolio = async () => {
-    const developer_info_getter = await fetch(
-      "http://localhost:5000/developer_info/6450cb8a8eb415ba6bd72ae9"
+    console.log(id);
+    const res = await fetch(
+      `http://192.168.0.104:5000/api/v1/developer/${id}`,
+      { method: "GET" }
     ).then((res) => res.json());
-    const service_getter = await fetch(
-      "http://localhost:5000/service/6450cb8a8eb415ba6bd72ae9"
-    ).then((res) => res.json());
-    const project_getter = await fetch(
-      "http://localhost:5000/project/6450cb8a8eb415ba6bd72ae9"
-    ).then((res) => res.json());
-    const testimonial_getter = await fetch(
-      "http://localhost:5000/testimonial/6450cb8a8eb415ba6bd72ae9"
-    ).then((res) => res.json());
-    console.log(developer_info_getter);
-    console.log(service_getter);
-    console.log(project_getter);
-    console.log(testimonial_getter);
-    const developer_info = developer_info_getter.data;
-    const service = service_getter.data;
-    const project = project_getter.data;
-    const testimonial = testimonial_getter.data;
-    setData({ ...data, developer_info, service, project, testimonial });
-  };
-  useEffect(() => {
-    getPortfolio();
-  }, []);
-  const i_id = "6450cb8a8eb415ba6bd72ae9";
-  const getProfileImg = async (e) => {
-    try {
-      const req = await instance.get(`/file/${i_id}`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      console.log(req);
-    } catch (err) {
-      console.log(err);
-      // navigate('/reports');
-    }
-  };
-  const getUserId = async (userName) => {
-    try {
-      const req = await instance.get(`/developer_info/fetchId?userName=${userName}`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      console.log(req);
-    } catch (err) {
-      console.log(err);
-      // Handle errors as needed
-    }
+    setData(res);
   };
 
   useEffect(() => {
-    getProfileImg();
-    getUserId("shuja");
+    if (id) {
+      // Check if id is not empty before fetching
+      getPortfolio();
+    }
+  }, [id]); // Added id as a dependency
+
+  useEffect(() => {
+    setId(location.pathname.split("/").pop());
   }, []);
 
   useEffect(() => {
@@ -81,7 +45,7 @@ function App() {
       <CustomCursor />
       <BackgroundImage />
       <Routes>
-        <Route path="/" element={<GeneralLayout />} />
+        <Route path="/:id" element={<GeneralLayout />} />
         <Route path="/case-study" element={<GeneralLayout />} />
       </Routes>
     </div>
