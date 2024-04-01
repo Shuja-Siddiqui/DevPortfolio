7a7481f7076825754100ad64c67b41ac32ec469c
Shuja Ur Rehman <shuja0094@gmail.com>
2024-04-01 11:34:38 +0500

    Update index.jsx

--- src/components/BackgroundImage/index.jsx
+++ src/components/BackgroundImage/index.jsx
@@ -2,6 +2,7 @@ import "./Background.css";
 
 export const BackgroundImage = () => {
   return (
+    <div className="w-full  h-screen flex justify-between items-start fixed top-0 left-0  z-10">
       <div className="w-[2px] border border-dotted border-night-black h-screen relative"></div>
       <div className="w-[2px] border border-dotted border-night-black h-screen relative">
         <div style={{ "--isci": "30s" }} className="animate-custom"></div>
