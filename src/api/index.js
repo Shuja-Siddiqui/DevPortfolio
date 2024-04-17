685392b2ac161653cc469ea93a600a8176cf165a
Shuja-Siddiqui <shuja0094@gmail.com>
2024-04-17 20:26:15 +0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ const env = process.env.REACT_APP_ENV || "local";
 const instance = axios.create({
   baseURL:
     env === "production"
-      ? "https://portfolio-api-zeta-seven.vercel.app/api/v1"
+      ? "http://localhost:5000/api/v1"
       : "http://localhost:5000/",
   headers: {
     "Content-Type": "application/json", // Set default headers
